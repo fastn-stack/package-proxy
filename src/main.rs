f1dcc24857e7b62ca0e8b7af7d9fed124d50d0f6
Amit Upadhyay <upadhyay@gmail.com>
2023-10-22 12:58:03 +0530

    adding fastn-observer

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,31 @@
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Debug)]
 struct Query {
     url: String,
 }
 
+#[tracing::instrument]
 #[actix_web::get("/")]
 async fn index() -> impl actix_web::Responder {
     "ok"
 }
 
+#[tracing::instrument(skip(query))]
 #[actix_web::get("/proxy/")]
 async fn proxy(query: actix_web::web::Query<Query>) -> impl actix_web::Responder {
-    println!("proxying {}", query.url);
-    let start = std::time::Instant::now();
-    let res = reqwest::get(&query.url)
+    tracing::info!(url = ?query.url);
+
+    reqwest::get(&query.url)
         .await
         .unwrap()
         .text()
         .await
-        .unwrap();
-    print_end("done", start);
-    res
-}
-
-pub fn print_end(msg: &str, start: std::time::Instant) {
-    use colored::Colorize;
-
-    if fastn_core::utils::is_test() {
-        println!("done in <omitted>");
-    } else {
-        println!(
-            // TODO: instead of lots of spaces put proper erase current terminal line thing
-            "\r{:?} {} in {:?}.                          ",
-            std::time::Instant::now(),
-            msg.green(),
-            start.elapsed()
-        );
-    }
+        .unwrap()
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    fastn_observer::observe();
+
     let host = std::env::var("HOST").unwrap_or_else(|_| "0.0.0.0".to_string());
     let port = std::env::var("PORT")
         .unwrap_or_else(|_| "8080".to_string())
